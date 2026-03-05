d4a9cd20b23799dfd5ae706fb50aefd4b050460c
JoaoPedroaac <joao.cintra7@hotmail.com>
2026-03-05 20:29:38 -0300

    feat(auth): adicionar login com google
    adiciona autenticação via oauth2 para login com google.
    inclui função simulada para retorno de sucesso.
    
    closes #1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log('sistema iniciado');
 
 
 function loginGoogle() {
-    return "login realizado com sucesso via google oauth2"
+    return "login realizado com sucesso via google oauth2..."
 }
 
 module.exports = loginGoogle;
