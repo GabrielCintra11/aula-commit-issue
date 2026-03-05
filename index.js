867d3186b4242ac5e164f20a18101123ec284940
JoaoPedroaac <joao.cintra7@hotmail.com>
2026-03-05 20:22:03 -0300

    feat(auth): Adicionar login com Google\

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-"console.log('sistema iniciado');" 
+console.log('sistema iniciado'); 
+
+
+function loginGoogle() {
+    return "login realizado com sucesso via google oauth2"
+}
+
+module.exports = loginGoogle;
